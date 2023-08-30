b16313e6c0094e645a7cf4d81565ea93690f9621
Signe Reimets <signereimets98@gmail.com>
2023-08-30 17:35:49 +0300

    Tegin klassi pangasüsteem ning lisasin erinevaid funktsioone

---
+++ homeworks/homework-2/banking_system.py
@@ -0,0 +1,40 @@
+class Banking_System:
+
+    def __init__(self, users, admin, money, workers):
+        self.__users = users
+        self.__admin = admin
+        self.__money = money
+        self.__workers = workers
+
+    def manage_webpage(self):
+        pass
+
+    def transfer(self):
+        pass
+
+    def insurance(self):
+        pass
+
+    def loan(self):
+        pass
+
+    def saving_account(self):
+        pass
+
+    def investment(self):
+        pass
+
+    def pension(self):
+        pass
+
+    def account_summary(self):
+        pass
+
+    def become_customer(self):
+        pass
+
+    def campaign(self):
+        pass
+
+    def user_identification(self):
+        pass
